343b01b484e94824cbcdcceccae08d1548944a2f
philmphoenix <jpholzmann@gmail.com>
2016-01-12 11:38:11 -0700

    Adds sockets to server side.
    Check out the implementation of the io.sockets.emit
    you can use that to make a chat room.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+io.on('connection', function(socket) {
+  console.log('Someone has connected.');
+
+  socket.on('message', function (channel, message) {
+    io.sockets.emit('chat message', message)
+  });
+});
+
 http.listen(process.env.PORT || 3000, function(){
   console.log('Your server is up and running on Port 3000. Good job!');
 });
